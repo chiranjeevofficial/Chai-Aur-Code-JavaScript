fa6204ebab61956e3a1432212b647bef7076ef1c
Chiranjeev Kashyap <chiranjeevkashyap060@gmail.com>
2023-10-01 19:50:39 +0530

    02 variable

---
+++ 01-Basic/02-variable.js
@@ -0,0 +1,12 @@
+const accountId = 14453;
+let accountEmail = "namaste@gmail.com"
+var accountPassword = "Namaste@123";
+accountCity = "Rishikesh";
+
+// accountId = 15015; //TypeError: Assignment to constant variable.
+accountEmail = "narayan@dev.com"
+accountPassword = "Narayan@123";
+accountCity = "Dehradun";
+
+// console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity]);
