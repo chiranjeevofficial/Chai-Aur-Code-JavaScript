94efc973c900fd43ebfc399f7bd7e9bedeec52a1
Chiranjeev Kashyap <chiranjeevkashyap060@gmail.com>
2023-10-01 21:05:25 +0530

    03 dataTypes

---
+++ 01-Basic/03-dataType.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3); // we're using nodeJS, not browser
+// console.log(3 + 3); // code readability should be high
+
+let name = "Chiranjeev";
+let age = 18;
+let isLoggedIn = false;
+
+// primitive dataTypes
+// number =======> 2^53
+// bigInt =======>
+// string =======> ""
+// boolean ======> true/false
+// null =========> standalone value | object
+// undefined ====> 
+// symbol =======> unique
+
+
+// object
+console.log(typeof "");
+console.log(typeof age);
+console.log(typeof null);
