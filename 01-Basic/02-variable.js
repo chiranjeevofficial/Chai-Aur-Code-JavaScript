60984bb3d520e42361e01ef353c5464f30886b62
Chiranjeev Kashyap <87925969+chiranjeevofficial@users.noreply.github.com>
2023-10-13 05:04:05 +0000

    02 variables update

--- 01-Basic/02-variable.js
+++ 01-Basic/02-variable.js
@@ -2,6 +2,7 @@ const accountId = 14453;
 let accountEmail = "namaste@gmail.com"
 var accountPassword = "Namaste@123";
 accountCity = "Rishikesh";
+let accountState;
 
 // accountId = 15015; //TypeError: Assignment to constant variable.
 accountEmail = "narayan@dev.com"
@@ -9,4 +10,4 @@ accountPassword = "Narayan@123";
 accountCity = "Dehradun";
 
 // console.log(accountId);
-console.table([accountEmail, accountPassword, accountCity]);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
