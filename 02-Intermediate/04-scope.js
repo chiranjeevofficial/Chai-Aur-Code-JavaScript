d4418780b1ad39d3f0029b33827a69522576b4ad
pradeeptiwaricode <68288547+pradeeptiwaricode@users.noreply.github.com>
2023-10-19 15:14:16 +0530

    2.4.1 scope

---
+++ 02-Intermediate/04-scope.js
@@ -0,0 +1,30 @@
+/*
+    1. its the block scope
+    {
+        code
+        code
+    }
+    2. outside the block scope is the global scope
+*/
+
+// const a = 10;
+// let b = 20;
+// var c = 30;
+
+// var c = 300;
+// if (true) {
+//     const a = 10;
+//     let b = 20;
+//     var c = 30;
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let a  = 23;
+// if (true) {
+    // let a = 10;
+    // console.log(`Inner a : ${a}`); // 10
+// }
+// console.log(`Outer a : ${a}`); // 23
