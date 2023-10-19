b0cc3abd6080bc5fb773a84e0800dfb087250ad2
pradeeptiwaricode <68288547+pradeeptiwaricode@users.noreply.github.com>
2023-10-19 15:40:47 +0530

    2.4.2

--- 02-Intermediate/04-scope.js
+++ 02-Intermediate/04-scope.js
@@ -28,3 +28,26 @@
     // console.log(`Inner a : ${a}`); // 10
 // }
 // console.log(`Outer a : ${a}`); // 23
+
+// ===================================== scope 2 =====================================
+function one() {
+    let username = "chiranjeev";
+    function two() {
+        let website = "youtube.com";
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+// one();
+
+// console.log(addOne(5));;
+function addOne(num) {
+    return num + 1;
+}
+
+// addTwo(8);
+let addTwo = function (num) {
+    return num + 2;   
+}
+
