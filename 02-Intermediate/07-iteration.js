a1fdbdbe3af21b8cb37357c42aff0ccd20f12b8c
pradeeptiwaricode <68288547+pradeeptiwaricode@users.noreply.github.com>
2023-10-23 17:11:09 +0530

    Update 07-iteration.js

--- 02-Intermediate/07-iteration.js
+++ 02-Intermediate/07-iteration.js
@@ -1,5 +1,5 @@
 // iteration with break and continue
-// for loop
+// ====================== for loop ====================== 
 // for (let i = 0 ; i <= 10 ; i++) {
     // console.log(`Namaste ${i} time`);
     // if (i == 7) {
@@ -13,7 +13,7 @@
     // console.log(`${i}`);
 // }
 
-// while and do-while loop
+// ====================== while and do-while loop ====================== 
 // let index = 0;
 // while (index <= 10) {
     // console.log(`Value of Index is: ${index}`);
@@ -29,9 +29,24 @@
     // index++;
 // }
 
-// let score = 1;
-let score = 11;
-do {
-    console.log(`Score is ${score}`);
-    score++;
-} while (score <= 10);
+// let score = 11;
+// do {
+    // console.log(`Score is ${score}`);
+    // score++;
+// } while (score <= 10);
+
+// ====================== for-of loop ====================== 
+// ["","",""] handle the string
+// [{},{},{}] handle the object
+
+// const arr = [1, 3, 5, 7, 9];
+// for (let num of arr) {
+//     console.log(num);
+// }
+
+// const greeting = "Namaste! Javascript";
+// for (const greet of greeting) {
+    // console.log(greet);
+// }
+
+//  ====================== MAP ====================== 
